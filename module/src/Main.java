30acdb151e081e4c2c52dc3d7c61d1971896e4f0
ssato01 <sato.shin01@genetec.co.jp>
2026-04-30 16:38:44 +0900

    Main.javaの関数isLeapYear()がint型の引数を取るように修正

--- module/src/Main.java
+++ module/src/Main.java
@@ -1,9 +1,42 @@
-class Main {
-    private static final int year = 2000;
-    public static boolean isLeapYear() {
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+class CheckYear {
+    protected final int year;
+
+    public CheckYear(int year) {
+        this.year = year;
+    }
+    public static boolean isLeapYear(int year) {
         return (year % 4 == 0) && (year % 100 != 0 || year % 400 == 0);
     }
+}
+
+class Main {
     static void main() {
-        System.out.println(year + "年はうるう年か：" + isLeapYear());
+        CheckYear checkyear;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("うるう年判定を行います。年を入力してください");
+
+        while(true) {
+            try {
+                int stdin = sc.nextInt();
+                if (stdin >= 1) {
+                    checkyear = new CheckYear(stdin);
+                    break;
+                }
+                else {
+                    System.out.println("年は正の整数で入力してください");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("年は数値(正の整数)で入力してください");
+                sc.next();
+            }
+        }
+
+        System.out.println(checkyear.year + "年はうるう年か："
+                + CheckYear.isLeapYear(checkyear.year));
+        sc.close();
+
     }
 }
