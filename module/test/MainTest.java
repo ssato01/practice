39586539a875c3586b1644ad6992f3982b83dcd4
ssato01 <sato.shin01@genetec.co.jp>
2026-04-30 17:47:31 +0900

    テスト内容の更新

--- module/test/MainTest.java
+++ module/test/MainTest.java
@@ -4,6 +4,10 @@ class MainTest {
 
     @org.junit.jupiter.api.Test
     void isLeapYear() {
-        assertEquals(true, Main.isLeapYear());
+        CheckYear checkyear = new CheckYear(0);
+        assertTrue(checkyear.isLeapYear(2028));
+        assertFalse(checkyear.isLeapYear(2026));
+        assertTrue(checkyear.isLeapYear(2000));
+        assertFalse(checkyear.isLeapYear(2100));
     }
 }
