a7b40224f11e8057b355a64def11daee00cda8bf
ssato01 <sato.shin01@genetec.co.jp>
2026-05-01 11:11:41 +0900

    isLeapYear()のstatic化

--- module/src/Main.java
+++ module/src/Main.java
@@ -1,42 +1,31 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-class CheckYear {
-    protected final int year;
-
-    public CheckYear(int year) {
-        this.year = year;
-    }
-    public boolean isLeapYear(int year) {
-        return (year % 4 == 0) && (year % 100 != 0 || year % 400 == 0);
-    }
+public static boolean isLeapYear(int year) {
+    return (year % 4 == 0) && (year % 100 != 0 || year % 400 == 0);
 }
 
-class Main {
-    static void main() {
-        CheckYear checkyear;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("うるう年判定を行います。年を入力してください");
+void main() {
+    int year;
+    Scanner sc = new Scanner(System.in);
+    System.out.println("うるう年判定を行います。年を入力してください");
 
-        while(true) {
-            try {
-                int stdin = sc.nextInt();
-                if (stdin >= 1) {
-                    checkyear = new CheckYear(stdin);
-                    break;
-                }
-                else {
-                    System.out.println("年は正の整数で入力してください");
-                }
-            } catch (InputMismatchException e) {
-                System.out.println("年は数値(正の整数)で入力してください");
-                sc.next();
+    while(true) {
+        try {
+            year = sc.nextInt();
+            if (year >= 1) {
+                break;
             }
+            else {
+                System.out.println("年は正の整数で入力してください");
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("年は数値(正の整数)で入力してください");
+            sc.next();
         }
-
-        System.out.println(checkyear.year + "年はうるう年か："
-                + checkyear.isLeapYear(checkyear.year));
-        sc.close();
-
     }
+
+    System.out.println(year + "年はうるう年か：" + isLeapYear(year));
+    sc.close();
+
 }
