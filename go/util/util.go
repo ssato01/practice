47e8f55f6d45e84b46d6dd4fc324692293d272df
ssato01 <sato.shin01@genetec.co.jp>
2026-05-13 09:50:21 +0900

    isLeapYear()関数の仕様変更及びテストカバレッジの確認

--- go/util/util.go
+++ go/util/util.go
@@ -8,7 +8,20 @@ import (
 )
 
 func isLeapYear(year int) bool {
-	return (year%4 == 0) && (year%100 != 0 || year%400 == 0)
+
+	isLeap := false
+
+	if year%4 == 0 {
+		if year%100 != 0 {
+			isLeap = true
+		} else {
+			if year%400 == 0 {
+				isLeap = true
+			}
+		}
+	}
+
+	return isLeap
 }
 
 func main() {
