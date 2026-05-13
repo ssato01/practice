148cf8c5d3d8371e97d665e16593508a2c4d61de
ssato01 <sato.shin01@genetec.co.jp>
2026-05-13 13:18:13 +0900

    isLeapYear()の変数削除及び分岐冗長性の解消(再)

--- go/util/util.go
+++ go/util/util.go
@@ -12,10 +12,9 @@ func isLeapYear(year int) bool {
 	if year%4 == 0 {
 		if year%100 != 0 {
 			return true
-		} else {
-			if year%400 == 0 {
-				return true
-			}
+		}
+		if year%400 == 0 {
+			return true
 		}
 	}
 
